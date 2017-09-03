3c86c3a47893aa5f8f3944db203852fa988479bc
Trevor Satterwhite <trevorsatterwhite@Trevors-MBP.attlocal.net>
2017-09-02 20:32:42 -0500

    udpate

---
+++ README.md
@@ -0,0 +1,2 @@
+# LGHH
+SMU Coding Camp - Group Project (Lower Greenville Happy Hour)
---
+++ sample.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+// server.listen(server_port, server_ip_address, function () {
+//   console.log( "Listening on " + server_ip_address + ", port " + server_port )
+// });
+
+// 'use strict';
+
+// app.use(express.static('css')); // contains css
+// app.use(express.static('photos')); // contains images
+// app.use(express.static('javascript')); // contains images
+// app.use(express.static('views')); //contains mainpage.html
+// app.use(express.static('bower_components')); // contains bootstrap stuff
+
+ // from https://www.yelp.com/developers/v3/manage_
+
+// app.listen(8080, function() {
+//     console.log('Example app listening on port 8080!')
+// })
